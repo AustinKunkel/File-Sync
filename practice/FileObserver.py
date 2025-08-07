5c2f3f7cf4b2561dfe312e2d0813f06456e13d9c
YoItsUncle <austinkunkel85@gmail.com>
2025-08-07 00:00:19 -0400

    Still in progress, but reading inbox on a seperate thread

--- practice/FileObserver.py
+++ practice/FileObserver.py
@@ -2,19 +2,21 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileMovedEvent
 import time
 from pathlib import Path
-from comparison_logic import compare_folder_hash
 from tracker_utils import save_tracked_paths
-from file_indexer_hasher import save_index_to_file, load_index_from_file, generate_file_hash
 from ssh_utils import file_event_queue
 import traceback
 from FileActions import FileAction
 import settings_util
 import shutil
+import threading
+from queue import Queue, Empty
 
 # Time to wait to verify file is stable (not being written anymore)
-STABLE_WAIT = 1.0
+STABLE_WAIT = 1
 
+inbox_queue = Queue()
 file_skip_dict = dict() # dictionary of files for the observer to skip (must be skipped when receiving) [filepath] -> timestamp
+TIME_TO_HANDLE_FILE = 2 # seconds it takes from last change to handle a file if it is inside of the skip dictionary
 
 class ChangeHandler(FileSystemEventHandler):
   def __init__(self, tracked_paths):
@@ -26,23 +28,21 @@ class ChangeHandler(FileSystemEventHandler):
     # If the file is in the inbox folder
     if event.src_path.startswith(str(self.inbox_path)):
       absolute_path = Path(event.src_path)
-      try:
-        relative_path = absolute_path.relative_to(self.inbox_path)
-      except ValueError:
-        relative_path = absolute_path
-
-      file_skip_dict[relative_path] = time.time() # make sure the file gets skipped when the system picks up whatever change it gets
-      self.handle_on_inbox(absolute_path, FileAction.SEND_FILE)
+      inbox_queue.put((absolute_path, time.time(), FileAction.SEND_FILE)) # make sure the file gets handled by the inbox checker
       return
     
     path = Path(event.src_path)
-
+    
     # If the file was moved from the inbox folder, check how long ago 
     if path in file_skip_dict.keys():
       current_time = time.time()
-      old_time = file_skip_dict[path]
+      old_time = file_skip_dict[path] # last update time
+      if current_time - old_time >= 2:
+        # Enough time has passed where we can send the file to remote
+        del file_skip_dict[path]
+      else:
+        return # dont handle the file
 
-    
     for tracked_path, folder_info in self.tracked_paths.items():
       folder_path = Path(tracked_path)
       # Check if the changed file is inside a tracked folder
@@ -58,7 +58,6 @@ class ChangeHandler(FileSystemEventHandler):
           traceback.print_exc()
           
         break
-    save_tracked_paths(self.tracked_paths)
 
   def on_created(self, event):
     self.on_modified(event)  # You can treat creation same as modification
@@ -70,26 +69,43 @@ class ChangeHandler(FileSystemEventHandler):
     if isinstance(event, FileMovedEvent):
       pass
 
-  def handle_on_inbox(self, src_path, action : FileAction):
+threading_stop_event = threading.Event()
 
-    if action == FileAction.SEND_FILE:
-      # ensure the path is inside the inbox
-      try:
-        rel_path = src_path.relative_to(settings_util.settings['local_inbox'])
-      except ValueError:
-        print(f"Path {src_path} is not inside the inbox base.")
+def check_inbox_worker(tracked_paths):
+  """Checks the file skip dictionary to see if there are any files in the inbox. If so, then handle them"""
+  while not threading_stop_event.is_set():
+    try:
+      src_path, last_update_time, action = inbox_queue.get(timeout=1)
+
+      # Ensures enough time has passed to handle the file
+      current_time = time.time()
+      if current_time - last_update_time <= STABLE_WAIT:
         return
       
-      dest_path = Path("/") / rel_path
-      # Ensure destination directory exists
-      dest_path.parent.mkdir(parents=True, exist_ok=False)
+      if action == FileAction.SEND_FILE:
+        # ensure the path is inside the inbox
+        try:
+          rel_path = src_path.relative_to(settings_util.settings['local_inbox'])
+        except ValueError:
+          print(f"Path {src_path} is not inside the inbox base.")
+          return
+        
+        dest_path = Path("/") / rel_path
+        # Ensure destination directory exists
+        dest_path.parent.mkdir(parents=True, exist_ok=True)
 
-      try:
-        shutil.move(str(src_path), str(dest_path))
-        print(f"Moved {src_path} -> {dest_path}")
-      except Exception as e:
-        print(f"Failed to move file: {e}")
-        traceback.print_exc(e)
+        try:
+          file_skip_dict[rel_path] = current_time
+          shutil.move(str(src_path), str(dest_path))
+          print(f"Moved {src_path} -> {dest_path}")
+        except Exception as e:
+          print(f"Failed to move file: {e}")
+          traceback.print_exc(e)
+    except Empty:
+      continue
+    threading_stop_event.set()
+    save_tracked_paths(tracked_paths)
+    time.sleep(.1)
 
 def start_monitoring(tracked_paths):
   event_handler = ChangeHandler(tracked_paths)
@@ -101,11 +117,18 @@ def start_monitoring(tracked_paths):
   inbox_path = settings_util.settings['local_inbox']
   observer.schedule(event_handler, inbox_path, recursive=True)
 
+  check_inbox_thread = threading.Thread(target=check_inbox_worker, args=(tracked_paths),daemon=True)
+  check_inbox_thread.start()
+
   observer.start()
+
   print("Monitoring started. Press Ctrl+C to stop.")
   try:
     while True:
       time.sleep(1)
   except KeyboardInterrupt:
     observer.stop()
+  print("Cleaning up...")
+  threading_stop_event.set()
+  check_inbox_thread.join()
   observer.join()
